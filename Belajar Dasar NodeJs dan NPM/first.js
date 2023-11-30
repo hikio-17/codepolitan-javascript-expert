4d34a251ed24da616f7c21153caa82d515ff2682
hikio-17 <fajri010217@gmail.com>
2023-12-01 00:17:32 +0700

    create fist file js

---
+++ Belajar Dasar NodeJs dan NPM/first.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 10; i++) {
+   console.log('Hello world from JS file');
+}
+
+const abc = 123;
+
+if (abc === 123) {
+   console.log('Betul');
+} else {
+   console.log('Salah');
+}
