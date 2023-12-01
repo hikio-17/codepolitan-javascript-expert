38d5ed8245d8259b5ba00d236db0f0f0efb4e096
hikio-17 <fajri010217@gmail.com>
2023-12-01 08:42:31 +0700

    learn exports module in nodejs

---
+++ Belajar Dasar NodeJs dan NPM/module/app.js
@@ -0,0 +1,7 @@
+const { luas, keliling } = require("./rumus");
+
+const luasSegitiga1 = luas(3);
+const kelilingSegitiga1 = keliling(3);
+
+console.log(luasSegitiga1);
+console.log(kelilingSegitiga1);
