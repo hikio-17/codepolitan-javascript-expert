6a900a990a5f02e6a818382f7f0de9a059e53791
hikio-17 <fajri010217@gmail.com>
2024-01-29 19:13:22 +0700

    add: mengatur posisi folder views agar tidak error

--- Belajar-Dasar-Expressjs/Expressjs-EJS/index.js
+++ Belajar-Dasar-Expressjs/Expressjs-EJS/index.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
    res.render('home');
