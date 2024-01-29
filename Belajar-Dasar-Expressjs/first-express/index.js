55bf9a3bacf6d1ff86e3b1a22b715a4aff5a6256
hikio-17 <fajri010217@gmail.com>
2024-01-29 11:58:44 +0700

    add: memisahkan router

--- Belajar-Dasar-Expressjs/first-express/index.js
+++ Belajar-Dasar-Expressjs/first-express/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-   res.send('ok');
+app.get('/', (req, res) => {
+   res.send('This is Homepage');
+});
+
+app.get('/cats', (req, res) => {
+   res.send('This is carts page');
+});
+
+app.get('/about', (req, res) => {
+   res.send('This is about page');
+});
+
+app.get('*', (req, res) => {
+   res.send('Halaman tidak ditemukan');
 });
 
 app.listen(8080, () => {
    console.log('server running at http://localhost:8080');
 });
+
